c51e6c16c89cd938257de8b01067aabf5087bf31
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2021-04-12 00:26:05 -0400

    Update App.svelte
    add postcss attribute to style block (fixes bundling issue)

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style global lang="postcss">
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
